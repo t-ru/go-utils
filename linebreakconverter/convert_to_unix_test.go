4cfff825e403ab584ae68e5e846c246e8a60ed85
Thomas Rudolph <thomas.rudolph@gmail.com>
2022-09-24 12:42:34 +0200

    Test for ConvertLinebreakToUnix added

---
+++ linebreakconverter/convert_to_unix_test.go
@@ -0,0 +1,75 @@
+/*
+MIT License
+
+Copyright (c) 2022 Thomas Rudolph
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package linebreakconverter
+
+import "testing"
+
+func TestConvertLinebreakToUnix(t *testing.T) {
+	tests := []struct {
+		name string
+		in   string
+		want string
+	}{
+		// TODO: Add test cases.
+		{"Test---001", "", ""},
+
+		{"Test---002", "\r\n", "\n"},
+		{"Test---003", "\r", "\n"},
+		{"Test---004", "\n", "\n"},
+
+		{"Test---005", "hello world\r\n", "hello world\n"},
+		{"Test---006", "hello world\r", "hello world\n"},
+		{"Test---007", "hello world\n", "hello world\n"},
+
+		{"Test---008", "\r\nhello world", "\nhello world"},
+		{"Test---009", "\rhello world", "\nhello world"},
+		{"Test---010", "\nhello world", "\nhello world"},
+
+		{"Test---011", "hello\r\nworld", "hello\nworld"},
+		{"Test---012", "hello\rworld", "hello\nworld"},
+		{"Test---013", "hello\nworld", "hello\nworld"},
+
+		{"Test---015", "hello,\r\n\r\nworld", "hello,\n\nworld"},
+		{"Test---016", "hello,\r\rworld", "hello,\n\nworld"},
+		{"Test---017", "hello,\n\nworld", "hello,\n\nworld"},
+	}
+	for _, tt := range tests {
+
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, _, err := ConvertLinebreakToUnix(tt.in)
+
+			if err != nil {
+				t.Errorf("\ntransforming error\n   in: %q\n   err: %v", tt.in, err)
+				return
+			}
+
+			if got != tt.want {
+				t.Errorf("\ntransforming got <> want\n   in: %q\n   got: %q\n   want: %q", tt.in, got, tt.want)
+			}
+
+		})
+	}
+}
